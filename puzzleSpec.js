851dc696869ab89f8ae2cb8e82d20a31a7ca37b6
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-01-22 17:15:11 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,8 +1,15 @@
 let punterPuzzleSpec = {
 	number: 151,
-	solveBy: "31 Dec",
+	solveBy: "28 Jan",
 	words: ["SKINT", "SLANT", "SLAIN", "AGAIN"],
-	hintSpec: [2, 3, "I"]
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"Just not getting lost required all Santa's skill ...<br>\
+		With street signs and numbers more difficult still.<br>\
+		He tangled in tree-tops <strong>AGAIN</strong> and <strong>AGAIN</strong>,<br>\
+		And barely missed hitting a tri-motored plane.<br>\
+		<br>\
+		From a draft of 'Rudolph, The Red-Nosed Reindeer' by Robert L. May, 1939."
 };
 
 
