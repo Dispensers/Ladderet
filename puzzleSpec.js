04a922b5cc8d6851d3b7165340efa5257aea367b
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-01-28 18:17:17 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,15 +1,92 @@
+
 let punterPuzzleSpec = {
-	number: 151,
+	number: 11,
+	solveBy: "4 Feb",
+	words: ["SIEGE", "SINGS", "PANGS", "PANIC"],
+	hintSpec: [2, 2, "N"],
+	featuredWordBlurb:
+		"The word PANIC is derived from the god Pan. " +
+		"The ancient Greeks believed that he lurked in lonely spots, and would frighten people either by suddenly appearing or by making noises."
+};
+
+
+
+
+
+
+/*
+let punterPuzzleSpec = {
+	number: 16,
 	solveBy: "28 Jan",
-	words: ["SKINT", "SLANT", "SLAIN", "AGAIN"],
+	words: ["GRAFT", "GRAZE", "GAUZE", "MAUVE"],
 	hintSpec: [2, 3, "I"],
 	featuredWordBlurb:
-		"Just not getting lost required all Santa's skill ...<br>\
-		With street signs and numbers more difficult still.<br>\
-		He tangled in tree-tops <strong>AGAIN</strong> and <strong>AGAIN</strong>,<br>\
-		And barely missed hitting a tri-motored plane.<br>\
-		<br>\
-		From a draft of 'Rudolph, The Red-Nosed Reindeer' by Robert L. May, 1939."
+		"This is puzzle number 16, so can you think of 16 5-letter colours?"
 };
+*/
+/*
+let punterPuzzleSpec = {
+	number: 21,
+	solveBy: "28 Jan",
+	words: ["SWIPE", "SWEPT", "INEPT", "INPUT"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"The World's Number One Baddie: INPUT?!"
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 26,
+	solveBy: "28 Jan",
+	words: ["SOBER", "SOLES", "SELLS", "HELLO"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"You say, 'Goodbye' and I say, 'HELLO, HELLO, HELLO'"
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 31,
+	solveBy: "28 Jan",
+	words: ["JOULE", "JOWLS", "JOKES", "YIKES"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"JOULE. Erg!! One of those incomprehensible units of measurement physics has dreamed up. That subject is hard work, don't you agree?"
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 36,
+	solveBy: "28 Jan",
+	words: ["FERRY", "WORRY", "WORLD", "WOWED"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"What’s your favourite Bryan FERRY or Roxy Music song? Mine is ‘Avalon’."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 41,
+	solveBy: "28 Jan",
+	words: ["BITTY", "PETTY", "PETAL", "SEPAL"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		" A bit of Botany.<br>" +
+		"Picture a rosebud. The green casing you see is a calyx. It's divided into segments called SEPALs, which spread out as the flower blooms."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 46,
+	solveBy: "28 Jan",
+	words: ["TANGO", "TABOO", "TABLE", "AMBLE"],
+	hintSpec: [2, 3, "I"],
+	featuredWordBlurb:
+		"TANGO: head snaps, sharp movements, sudden pauses and ‘ganchos’ (leg hooks). Out of Buenos Aires and Montevideo in the 1880s. " +
+		"Many variants now, but Argentine TANGO is still very popular."
+};
+*/
+
+
 
 
