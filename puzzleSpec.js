0f84bb73611f1ef906c6a1fe866d6230edd04728
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-02-05 18:59:36 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,4 +1,4 @@
-
+/*
 let punterPuzzleSpec = {
 	number: 11,
 	solveBy: "4 Feb",
@@ -8,30 +8,25 @@ let punterPuzzleSpec = {
 		"The word PANIC is derived from the god Pan. " +
 		"The ancient Greeks believed that he lurked in lonely spots, and would frighten people either by suddenly appearing or by making noises."
 };
+*/
 
-
-
-
-
-
-/*
 let punterPuzzleSpec = {
 	number: 16,
-	solveBy: "28 Jan",
+	solveBy: "11 Feb",
 	words: ["GRAFT", "GRAZE", "GAUZE", "MAUVE"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [2, 4, "E"],
 	featuredWordBlurb:
 		"This is puzzle number 16, so can you think of 16 5-letter colours?"
 };
-*/
+
 /*
 let punterPuzzleSpec = {
 	number: 21,
 	solveBy: "28 Jan",
 	words: ["SWIPE", "SWEPT", "INEPT", "INPUT"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [1, 0, "S"],
 	featuredWordBlurb:
-		"The World's Number One Baddie: INPUT?!"
+		"The World's Number One Diebad: INPUT."
 };
 */
 /*
@@ -39,9 +34,9 @@ let punterPuzzleSpec = {
 	number: 26,
 	solveBy: "28 Jan",
 	words: ["SOBER", "SOLES", "SELLS", "HELLO"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [1, 0, "S"],
 	featuredWordBlurb:
-		"You say, 'Goodbye' and I say, 'HELLO, HELLO, HELLO'"
+		"\"You say, 'Goodbye' and I say, 'HELLO, HELLO, HELLO'\""
 };
 */
 /*
@@ -49,7 +44,7 @@ let punterPuzzleSpec = {
 	number: 31,
 	solveBy: "28 Jan",
 	words: ["JOULE", "JOWLS", "JOKES", "YIKES"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [2, 3, "E"],
 	featuredWordBlurb:
 		"JOULE. Erg!! One of those incomprehensible units of measurement physics has dreamed up. That subject is hard work, don't you agree?"
 };
@@ -59,7 +54,7 @@ let punterPuzzleSpec = {
 	number: 36,
 	solveBy: "28 Jan",
 	words: ["FERRY", "WORRY", "WORLD", "WOWED"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [1, 2, "R"],
 	featuredWordBlurb:
 		"What’s your favourite Bryan FERRY or Roxy Music song? Mine is ‘Avalon’."
 };
@@ -69,9 +64,9 @@ let punterPuzzleSpec = {
 	number: 41,
 	solveBy: "28 Jan",
 	words: ["BITTY", "PETTY", "PETAL", "SEPAL"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [2, 3, "A"],
 	featuredWordBlurb:
-		" A bit of Botany.<br>" +
+		" A bit of Botany.<br><br>" +
 		"Picture a rosebud. The green casing you see is a calyx. It's divided into segments called SEPALs, which spread out as the flower blooms."
 };
 */
@@ -80,7 +75,7 @@ let punterPuzzleSpec = {
 	number: 46,
 	solveBy: "28 Jan",
 	words: ["TANGO", "TABOO", "TABLE", "AMBLE"],
-	hintSpec: [2, 3, "I"],
+	hintSpec: [2, 4, "E"],
 	featuredWordBlurb:
 		"TANGO: head snaps, sharp movements, sudden pauses and ‘ganchos’ (leg hooks). Out of Buenos Aires and Montevideo in the 1880s. " +
 		"Many variants now, but Argentine TANGO is still very popular."
