25cf20eaaee36cb7eb791244a1b3d6256599f032
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-05-12 12:27:23 +0100

    Add files via upload

---
+++ puzzleSpecs.js
@@ -0,0 +1,158 @@
+let puzzleSpecs = [
+	{
+		number: 46,
+		publishedOn: "18 Mar 24",
+		words: ["TANGO", "TABOO", "TABLE", "AMBLE"],
+		hintSpec: [2, 4, "E"],
+		featuredWordBlurb:
+		"TANGO: head snaps, sharp movements, sudden pauses and ‘ganchos’ (leg hooks). Out of Buenos Aires and Montevideo in the 1880s. " +
+		"Many variants now, but Argentine TANGO is still very popular."
+	},
+	{
+		number: 31,
+		publishedOn: "11 Mar 24",
+		words: ["JOULE", "JOWLS", "JOKES", "YIKES"],
+		hintSpec: [2, 3, "E"],
+		featuredWordBlurb:
+			"JOULE. Erg!! One of those incomprehensible units of measurement physics has dreamed up. That subject is hard work, don't you agree?"
+	},
+	{
+		number: 41,
+		publishedOn: "4 Mar 24",
+		words: ["BITTY", "PETTY", "PETAL", "SEPAL"],
+		hintSpec: [2, 3, "A"],
+		featuredWordBlurb:
+			" A bit of Botany.<br><br>" +
+			"Picture a rosebud. The green casing you see is a calyx. It's divided into segments called SEPALs, which spread out as the flower blooms."
+	},
+	{
+		number: 36,
+		publishedOn: "26 Feb 24",
+		words: ["FERRY", "WORRY", "WORLD", "WOWED"],
+		hintSpec: [1, 2, "R"],
+		featuredWordBlurb:
+			"What’s your favourite Bryan FERRY or Roxy Music song? Mine is ‘Avalon’."
+	},
+	{
+		number: 26,
+		publishedOn: "19 Feb 24",
+		words: ["SOBER", "SOLES", "SELLS", "HELLO"],
+		hintSpec: [1, 0, "S"],
+		featuredWordBlurb:
+			"\"You say, 'Goodbye' and I say, 'HELLO, HELLO, HELLO'\""
+	},
+	{
+		number: 21,
+		publishedOn: "12 Feb 24",
+		words: ["SWIPE", "SWEPT", "INEPT", "INPUT"],
+		hintSpec: [2, 1, "N"],
+		featuredWordBlurb:
+			"The World's Number One Diebad: INPUT."
+	},
+	{
+		number: 16,
+		publishedOn: "5 Feb 24",
+		words: ["GRAFT", "GRAZE", "GAUZE", "MAUVE"],
+		hintSpec: [2, 4, "E"],
+		featuredWordBlurb:
+			"This is puzzle number 16, so can you think of 16 5-letter colours?"
+	},
+	{
+		number: 11,
+		publishedOn: "30 Jan 24",
+		words: ["SIEGE", "SINGS", "PANGS", "PANIC"],
+		hintSpec: [2, 2, "N"],
+		featuredWordBlurb:
+			"The word PANIC is derived from the god Pan. " +
+			"The ancient Greeks believed that he lurked in lonely spots, and would frighten people either by suddenly appearing or by making noises."
+	},
+	{
+		number: 10,
+		publishedOn: "18 Feb 22",
+		words: ["BRAVO", "CRAVE", "CURVE", "NURSE"],
+		hintSpec: [2, 4, "E"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 9,
+		publishedOn: "17 Feb 22",
+		words: ["OLIVE", "CLOVE", "CLOWN", "BROWN"],
+		hintSpec: [2, 2, "O"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 8,
+		publishedOn: "16 Feb 22",
+		words: ["DRIVE", "DRIES", "DIMES", "LIMOS"],
+		hintSpec: [1, 1, "R"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 7,
+		publishedOn: "15 Feb 22",
+		words: ["THINK", "CHINS", "CHESS", "GUESS"],
+		hintSpec: [2, 4, "S"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 6,
+		publishedOn: "14 Feb 22",
+		words: ["TEACH", "PERCH", "PERIL", "PUPIL"],
+		hintSpec: [1, 1, "E"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 5,
+		publishedOn: "11 Feb 22",
+		words: ["SMALL", "SMILE", "SLIME", "CLIMB"],
+		hintSpec: [1, 3, "L"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 4,
+		publishedOn: "10 Feb 22",
+		words: ["BIRDS", "TIRES", "TREES", "TWEET"],
+		hintSpec: [1, 4, "S"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 3,
+		publishedOn: "9 Feb 22",
+		words: ["BUILD", "GUILE", "GRIME", "FRAME"],
+		hintSpec: [2, 1, "R"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 2,
+		publishedOn: "8 Feb 22",
+		words: ["ELDER", "WIDER", "WIDTH", "WORTH"],
+		hintSpec: [2, 0, "W"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 1,
+		publishedOn: "7 Feb 22",
+		words: ["BASIL", "BAGEL", "ANGEL", "ANGST"],
+		hintSpec: [1, 0, "B"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+	{
+		number: 0,
+		publishedOn: "7 Feb 22",
+		words: ["DEBUT", "DEBTS", "VESTS", "VISTA"],
+		hintSpec: [1, 1, "E"],
+		featuredWordBlurb:
+			"There isn't one!."
+	},
+];
+
